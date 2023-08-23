7bc3a5a9555c737011b2d6077995c4aa8606d30c
kevin <kevinooi1997@gmail.com>
2023-08-23 16:50:19 +0800

    add twitter clone

---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,132 @@
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+  createTRPCContext,
+} from "~/server/api/trpc";
+
+export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      })
+    )
+    .query(async ({ input: { userId, limit = 10, cursor }, ctx }) => {
+      return await getInfiniteTweets({
+        whereClause: { userId },
+        limit,
+        cursor,
+        ctx,
+      });
+    }),
+  infiniteFeed: publicProcedure
+    .input(
+      z.object({
+        onlyFollowing: z.boolean().optional(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      })
+    )
+    .query(
+      async ({ input: { onlyFollowing = false, limit = 10, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfiniteTweets({
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: {
+                    followers: { some: { id: currentUserId } },
+                  },
+                },
+          limit,
+          cursor,
+          ctx,
+        });
+      }
+    ),
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      void ctx.revalidateSSG?.(`/profiles/${ctx.session.user.id}`);
+      const tweet = await ctx.prisma.tweet.create({
+        data: { content, userId: ctx.session.user.id },
+      });
+
+      return tweet;
+    }),
+  toggleLike: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input: { id }, ctx }) => {
+      const data = { tweetId: id, userId: ctx.session?.user.id };
+      const existingLike = await ctx.prisma.like.findUnique({
+        where: { userId_tweetId: data },
+      });
+
+      if (existingLike == null) {
+        await ctx.prisma.like.create({ data });
+        return { addedLike: true };
+      } else {
+        await ctx.prisma.like.delete({ where: { userId_tweetId: data } });
+        return { addedLike: false };
+      }
+    }),
+});
+
+async function getInfiniteTweets({
+  whereClause,
+  limit,
+  cursor,
+  ctx,
+}: {
+  whereClause?: Prisma.TweetWhereInput;
+  limit: number;
+  cursor: { id: string; createdAt: Date } | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user?.id ?? null;
+
+  const data = await ctx.prisma.tweet.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: { select: { name: true, id: true, image: true } },
+    },
+  });
+
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+
+  return {
+    tweets: data.map((tweet) => {
+      return {
+        id: tweet.id,
+        content: tweet.content,
+        createdAt: tweet.createdAt,
+        likeCount: tweet._count.likes,
+        user: tweet.user,
+        likedByMe: (tweet.likes?.length ?? 0) > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
